e4e67693764ecbaf323c4e0ee3c1fe250a226909
msp970818 <969017118@qq.com>
2018-08-27 16:46:08 +0800

    修复bug

--- App/Wepos/app/src/main/java/net/msp/kaituo/wepos/ui/activity/LoginActivity.java
+++ App/Wepos/app/src/main/java/net/msp/kaituo/wepos/ui/activity/LoginActivity.java
@@ -1,6 +1,5 @@
 package net.msp.kaituo.wepos.ui.activity;
 
-<<<<<<< HEAD
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -13,11 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-=======
-
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
->>>>>>> e4fc819e3f0582dd6af7cee514df39ac9bfd9c68
 
 import net.msp.kaituo.wepos.R;
 
---
+++ App/Wepos/app/src/main/java/net/msp/kaituo/wepos/ui/activity/WifiListContectActivity.java
@@ -0,0 +1,16 @@
+package net.msp.kaituo.wepos.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.msp.kaituo.wepos.R;
+
+
+public class WifiListContectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi_list_contect);
+    }
+}
